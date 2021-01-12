cf699fb04fbd46e6cc579b349fccf86aafc87cec
DESKTOP-UU8PU3U\danie <daniel.kuhman@kuhmantech.com>
2021-01-11 21:14:28 -0600

    Finished script for divergent bar plot

--- ggplot_divergent_barplot.R
+++ ggplot_divergent_barplot.R
@@ -10,28 +10,74 @@ mydata_path <- file.choose(new = FALSE)
 mydata <- read.csv(mydata_path)
 rm(mydata_path)
 
-# PREP DATA
-# Inverse female heights
-mydata <- mydata %>% 
-  mutate(Height = ifelse(Gender == 'Male',
-                         Height,
-                         -1*Height))
-
-# Calculate Break Values
-y_breaks <- pretty(mydata$Height)
-
-# PLOT
+# PLOT REGULAR GROUPED BAR PLOT
 ggplot(mydata,
        aes(x = Country,
            y = Height,
            fill = Gender)) +
   geom_bar(stat = 'identity',
+           position = position_dodge()) +
+  coord_flip()+
+  theme_classic() +
+  theme(
+    axis.title = element_text(size = 20,
+                              color = 'black',
+                              face = 'bold'),
+    axis.text = element_text(size = 14,
+                             color = 'black',
+                             face = 'bold'),
+    axis.line = element_line(size = 1.5),
+    legend.position = 'right',
+    legend.title = element_text(size = 16,
+                                color = 'black',
+                                face = 'bold'),
+    legend.text = element_text(size = 16,
+                               color = 'black',
+                               face = 'bold')
+  )
+
+# PREP DATA FOR DIVERGENT BARS
+mydata <- mydata %>% 
+  mutate(Height = ifelse(Gender == 'Male',
+                         Height,
+                         -1*Height))
+
+y_breaks <- pretty(mydata$Height)
+
+
+# PLOT DIVERGENT BAR PLOTS
+ggplot(mydata, 
+       aes(x = Country,
+           y = Height,
+           fill = Gender)) +
+  geom_bar(stat = 'identity',
            color = 'white',
-           lwd = 1.5) +
-  coord_flip() +
+           lwd = 2)+
+  coord_flip()+
   scale_y_continuous(breaks = y_breaks,
-                     labels = abs(y_breaks)) +
-  scale_fill_manual(values = c('#FF99CC',
-                               '#99CCFF')) + 
+                     labels = abs(y_breaks))+
   ylab('Height (cm)') +
-  theme_classic()
+  theme_classic() +
+  theme(
+    axis.title = element_text(size = 20,
+                              color = 'black',
+                              face = 'bold'),
+    axis.text = element_text(size = 14,
+                             color = 'black',
+                             face = 'bold'),
+    axis.line = element_line(size = 1.5),
+    legend.position = 'right',
+    legend.title = element_text(size = 16,
+                                color = 'black',
+                                face = 'bold'),
+    legend.text = element_text(size = 16,
+                               color = 'black',
+                               face = 'bold')
+  )
+
+
+
+
+
+
+
